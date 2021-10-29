c5a62673b129cfaaed1abfb720a06f1005da590f
AurelianoSabino <aure.sabino@gmail.com>
2021-10-29 20:22:39 -0300

    Aula 1 -> Criação model Person e Account

---
+++ account/src/main/java/br/com/lab/impacta/account/model/Account.java
@@ -0,0 +1,35 @@
+package br.com.lab.impacta.account.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private Long number;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "person_Id")
+    private Person customer;
+
+    private Double balance;
+
+    public boolean debit(Double valueDebit){
+
+        if(this.getBalance() < valueDebit){
+            return false;
+        }
+
+        Double debitAmount = this.getBalance() - valueDebit;
+        this.setBalance(debitAmount);
+        return true;
+    }
+
+
+}
